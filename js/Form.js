6cef3fbe44ac79975fc0b746797f8b637ea47932
mehakpreet1213 <72645356+mehakpreet1213@users.noreply.github.com>
2020-12-13 12:56:30 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,46 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+  //  this.input1 = createInput("password");
+    this.button = createButton('Play');
+    this.reset = createButton('reset');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(displayWidth/2,30);
+
+    this.input.position(displayWidth/2-10,80);
+   // this.input1.position(displayWidth/2-10,200);
+    this.button.position(displayWidth/2,120);
+    this.reset.position(displayWidth-50,50);
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2,displayHeight/2);
+    });
+
+        this.reset.mousePressed(()=>{
+        player.updateCount(0);                                           
+        game.update(0);  
+        Player.updatecaratend(0);                                                
+ 
+    }); 
+                       
+  }
+}
